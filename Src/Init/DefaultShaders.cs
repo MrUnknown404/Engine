517a555e90c116aaa054d88c95940e33ccfff8cf
-Unknown- <mrunknownyt@gmail.com>
2023-10-07 09:40:55 -0600

    Update DefaultShaders.cs

--- Src/Init/DefaultShaders.cs
+++ Src/Init/DefaultShaders.cs
@@ -11,8 +11,8 @@ namespace USharpLibs.Engine.Init {
 		public static Color4 DefaultFontColor => Color4.White;
 		public static Color4 DefaultOutlineColor => Color4.Black;
 
-		public static readonly UnboundShader<DefaultHudShader> DefaultHud = Add(new DefaultHudShader("DefaultHud") { AssemblyOverride = GameEngine.USharpEngineAssembly.Value, });
-		public static readonly UnboundShader<DefaultHudShader> DefaultFont = Add(new DefaultHudShader("DefaultFont") { AssemblyOverride = GameEngine.USharpEngineAssembly.Value, });
+		public static UnboundShader<DefaultHudShader> DefaultHud { get; } = Add(new DefaultHudShader("DefaultHud") { AssemblyOverride = GameEngine.USharpEngineAssembly.Value, });
+		public static UnboundShader<DefaultHudShader> DefaultFont { get; } = Add(new DefaultHudShader("DefaultFont") { AssemblyOverride = GameEngine.USharpEngineAssembly.Value, });
 
 		private static UnboundShader<T> Add<T>(T shader) where T : Shader {
 			UnboundShader<T> s = new(shader);
